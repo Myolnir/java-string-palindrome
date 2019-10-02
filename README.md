77cfc6b48e4cff91447c9d976683c4a442342b5b
angel <angel.muelas@denizen.io>
2019-10-02 12:32:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Palindrome string
+
+Program that checks if a given string is whether a palindrome or not.
+
+To check this instead of check the hole string I do not have to loop over the hole string
+but only the first half of it.
+
+We only have one for loop so the complexity of the solution is a O(n)
+
+## Instalation
+
+The class is situated on the package com.myolnir so to compile you execute
+
+`javac -d <where do you want to put the class files>/Palindrome.java`
+
+
+After a couple of seconds you will have a Palindrome.class in the route you put on the above command.
+
+To execute this class you should run:
+
+`java com.myolnir.Palindrome`
+
+The program should run and ask you for a string, after you enter the script the program returns if
+the given string is a palindrome or not.
+
---
+++ src/com/myolnir/Palindrome.java
@@ -0,0 +1,39 @@
+package com.myolnir;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        String original;
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter a Number or String: ");
+        original=in.nextLine();
+        if (isPalindrome(original)) {
+            System.out.println("The string is a palindrome");
+        } else {
+            System.out.println("The string is not a palindrome");
+        }
+
+    }
+
+
+    /**
+     * Checks if the given string is a palindrome.
+     *
+     * @param originalString the original string
+     * @return whether the given string a palindrome or not
+     */
+    public static boolean isPalindrome(String originalString) {
+        int stringLength = originalString.length();
+        int halfStringLength = stringLength/2;
+        int j = stringLength - 1;
+        for(int i=0; i<halfStringLength; i++) {
+            if(originalString.charAt(i) != originalString.charAt(j)) {
+                return false;
+            }
+            j--;
+        }
+        return true;
+    }
+}
